94f28a30d997945efe5aab0874f091b26cec859c
nguyendinhtrieu <nguyendinhtrieu365@gmail.com>
2023-12-27 17:50:10 +0700

    Init project: Generate project - Spring Data JPA - Lombok - Thymeleaf - Spring Security - WebSocket

---
+++ src/test/java/com/tzyel/springbaseproject/SpringBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzyel.springbaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
