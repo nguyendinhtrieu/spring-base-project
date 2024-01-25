5b31c090d4140afd4b37b64141a96b6b9abcf2d6
nguyendinhtrieu <nguyendinhtrieu365@gmail.com>
2024-01-25 14:38:47 +0700

    Adding logic thymeleaf layout

--- src/main/java/com/tzyel/springbaseproject/config/ApplicationConfig.java
+++ src/main/java/com/tzyel/springbaseproject/config/ApplicationConfig.java
@@ -1,11 +1,16 @@
 package com.tzyel.springbaseproject.config;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.apache.commons.codec.CharEncoding;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
@@ -26,5 +31,23 @@ public class ApplicationConfig {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.getDefault());
         return slr;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(templateResolver());
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(CharEncoding.UTF_8);
+        return templateResolver;
     }
 }
